c2ff16ae70c4ccfa96f96b06fe7f85be251b03b7
Travis Elkins <travis.elkins+github@gmail.com>
2023-08-16 11:47:40 +0200

    Changed role seeder to make it clearer what the roles' IDs are.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,6 +12,18 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Role::factory(10)->create();
+        \App\Models\Role::factory(10)
+            ->sequence(
+                ['name' => 'Role 1'],
+                ['name' => 'Role 2'],
+                ['name' => 'Role 3'],
+                ['name' => 'Role 4'],
+                ['name' => 'Role 5'],
+                ['name' => 'Role 6'],
+                ['name' => 'Role 7'],
+                ['name' => 'Role 8'],
+                ['name' => 'Role 9'],
+                ['name' => 'Role 10'],
+            )->create();
     }
 }
